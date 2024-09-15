ce0923a562c693cc7b0a8ab6de58d8b1f7a97fb8
Joannis Orlandos <joannis@orlandos.nl>
2024-09-15 15:34:53 +0300

    HB 2.0's release

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Articles", targets: ["Articles"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0.rc.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird-websocket.git", from: "2.0.0"),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0-beta.3"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.1"),
